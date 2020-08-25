3cbfd7caf494deff5ba86df47755ea2f92b79ff0
stimp <stimpon@gmail.com>
2020-08-25 19:57:28 +0200

    Registry check

--- WinCrypt/WinCrypt/Helpers/RegistryHelpers.cs
+++ WinCrypt/WinCrypt/Helpers/RegistryHelpers.cs
@@ -4,12 +4,13 @@
     using Microsoft.Win32;
     using System;
     using System.IO;
+    using System.Windows;
 
     public static class RegistryHelpers
     {
         /// <summary>
         /// Sets up the necessary registry keys and values
-        /// 
+        /// =================================================================
         /// This function will first check if the keys exists and will create
         /// them if they don't exist
         /// </summary>
@@ -17,50 +18,64 @@
         {
             try
             {
-                // Get the registry subkey for this program
-                RegistryKey key = Registry.ClassesRoot.OpenSubKey("WinCrypt");
-                // If it does not exist
-                if (key == null)
-                {
-                    // Then create the registry key
-                    var subKey = Registry.ClassesRoot.CreateSubKey("WinCrypt");
-                    // Set default icon
-                    subKey.CreateSubKey("DefaultIcon").SetValue("", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
+                #region Check keys
 
-                    // Create shell command
-                    var shellKey = subKey.CreateSubKey("shell").CreateSubKey("Decrypt");
-                    // Set command string
-                    shellKey.SetValue("", "Decrypt");
-                    // Set command icon
-                    shellKey.SetValue("Icon", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
-                    // Create command
-                    shellKey.CreateSubKey("Command").SetValue("", @"""" + Directory.GetCurrentDirectory() + @"\WinCrypt.exe"" ""%1"" ""1""");
-                }
-
+                // Get the registry subkey for the this program
+                RegistryKey keyWIN = Registry.ClassesRoot.OpenSubKey("WinCrypt");
                 // Get the registry subkey for the encrypted file extension
-                key = Registry.ClassesRoot.OpenSubKey(".encrypted");
-                // If it does not exist
-                if (key == null)
-                {
-                    // Create the .encrypted file extension registry key
-                    var subKey = Registry.ClassesRoot.CreateSubKey(".encrypted");
-                    // Accosiate the .encrypted file extension with this program
-                    subKey.SetValue("", "WinCrypt");
-                }
-
+                RegistryKey keyENC = Registry.ClassesRoot.OpenSubKey(".encrypted");
                 // Get registry subkey for the Encrypt command
-                key = Registry.ClassesRoot.OpenSubKey(@"*\shell", true);
-                // If it does not exist
-                if (key.OpenSubKey("Encrypt") == null)
+                RegistryKey keyCOM = Registry.ClassesRoot.OpenSubKey(@"*\shell", true).OpenSubKey("Encrypt");
+
+                #endregion
+
+                // Check if any keys are null
+                if (keyWIN == null || keyENC == null || keyCOM == null)
                 {
-                    // Create the encrypt key
-                    var subKey = key.CreateSubKey("Encrypt");
-                    // Set Encrypt icon
-                    subKey.SetValue("Icon", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
-                    // Create command
-                    var subsubkey = subKey.CreateSubKey("Command");
-                    // Set command value
-                    subsubkey.SetValue("", @"""" + Directory.GetCurrentDirectory() + @"\WinCrypt.exe"" ""%1"" ""0""");
+                    // Ask user if he/she wants to add the registry keys right now
+                    var userResult = MessageBox.Show("You are missing necessary registry keys, this program will not run correctly without them. Do you want to create them now?", "Missing registry keys", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                    // Check the user choice
+                    switch (userResult)
+                    {
+                        case MessageBoxResult.Yes:
+                            // Remove ecisting keys
+                            RegistryCleanup();
+
+                            #region Create all necessary subkeys
+                            // Then create the registry key
+                            var subKey1 = Registry.ClassesRoot.CreateSubKey("WinCrypt");
+                            // Set default icon
+                            subKey1.CreateSubKey("DefaultIcon").SetValue("", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
+
+                            // Create shell command
+                            var shellKey = subKey1.CreateSubKey("shell").CreateSubKey("Decrypt");
+                            // Set command string
+                            shellKey.SetValue("", "Decrypt");
+                            // Set command icon
+                            shellKey.SetValue("Icon", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
+                            // Create command
+                            shellKey.CreateSubKey("Command").SetValue("", @"""" + Directory.GetCurrentDirectory() + @"\WinCrypt.exe"" ""%1"" ""1""");
+
+                            // Create the .encrypted file extension registry key
+                            var subKey2 = Registry.ClassesRoot.CreateSubKey(".encrypted");
+                            // Accosiate the .encrypted file extension with this program
+                            subKey2.SetValue("", "WinCrypt");
+
+                            // Create the encrypt key
+                            var subKey3 = Registry.ClassesRoot.OpenSubKey(@"*\shell", true).CreateSubKey("Encrypt");
+                            // Set Encrypt icon
+                            subKey3.SetValue("Icon", Directory.GetCurrentDirectory() + @"\WinCrypt.exe,0");
+                            // Create command
+                            var subsubkey = subKey3.CreateSubKey("Command");
+                            // Set command value
+                            subsubkey.SetValue("", @"""" + Directory.GetCurrentDirectory() + @"\WinCrypt.exe"" ""%1"" ""0""");
+                            #endregion
+
+                            break;
+                        // Just return if the user chooses not to add the registry keys
+                        default: return;
+                    }
                 }
             }
             catch(Exception ex)
@@ -87,15 +102,15 @@
                     // If WinCrypt exist
                     if (key.OpenSubKey("WinCrypt") != null) 
                         // Delete the Registry Key
-                        key.DeleteSubKey("WinCrypt");
+                        key.DeleteSubKeyTree("WinCrypt");
                     // If .encrypted exist
                     if (key.OpenSubKey(".encrypted") != null)
                         // Delete the Registry Key
-                        key.DeleteSubKey(".encrypted");
+                        key.DeleteSubKeyTree(".encrypted");
                     // If Encrypt key exist
                     if (key.OpenSubKey(@"*\shell\Encrypt") != null)
                         // Delete the Registry Key
-                        key.DeleteSubKey(@"*\shell\Encrypt");
+                        key.DeleteSubKeyTree(@"*\shell\Encrypt");
                 }
             }
             // If it fails
